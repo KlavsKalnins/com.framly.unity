5ced12e214b0a7607cd1a83a508674c82aa397b5
klāvs kalniņš <klavskalninss@gmail.com>
2022-06-20 23:24:37 +0300

    FYVar Generic comments

--- Runtime/Scripts/Variables/Single/FYVar.cs
+++ Runtime/Scripts/Variables/Single/FYVar.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 namespace Framly
 {
+    // TODO: maybe can do generic type FYVar<T>
     public abstract class FYVar : ScriptableObject
     {
+        // public T value;
+        // but would be difficult to base class generic as i would have to set the type <int, stirng, etc.>
         public abstract string GetStringValue();
     }
 }
